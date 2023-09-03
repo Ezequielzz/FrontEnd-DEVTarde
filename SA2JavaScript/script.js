9db5f46bca9bedb1e7be8e922ed0ad104bf3de02
Ezequielzz <ezequielsamueldl@gmail.com>
2023-09-03 15:23:11 -0300

    90% (Falta Alguns Detalhes)

--- SA2JavaScript/script.js
+++ SA2JavaScript/script.js
@@ -1,57 +1,52 @@
+let autoModeInterval;
 
 // Ligando a Luz Vermelha
 function pare() {
-    document.querySelector('.vermelho').classList.remove('opacidade');
-    document.querySelector('.amarelo').classList.add('opacidade');
-    document.querySelector('.verde').classList.add('opacidade');
+    clearInterval(autoModeInterval);
+  document.querySelector(".vermelho").classList.remove("desligado");
+  document.querySelector(".amarelo").classList.add("desligado");
+  document.querySelector(".verde").classList.add("desligado");
 }
 
 // Ligando a Luz Amarela
 function atencao() {
-    document.querySelector('.vermelho').classList.add('opacidade');
-    document.querySelector('.amarelo').classList.remove('opacidade');
-    document.querySelector('.verde').classList.add('opacidade');
+    clearInterval(autoModeInterval);
+  document.querySelector(".vermelho").classList.add("desligado");
+  document.querySelector(".amarelo").classList.remove("desligado");
+  document.querySelector(".verde").classList.add("desligado");
 }
 
 // Ligando a Luz Verde
 function siga() {
-    document.querySelector('.vermelho').classList.add('opacidade');
-    document.querySelector('.amarelo').classList.add('opacidade');
-    document.querySelector('.verde').classList.remove('opacidade');
+    clearInterval(autoModeInterval);
+  document.querySelector(".vermelho").classList.add("desligado");
+  document.querySelector(".amarelo").classList.add("desligado");
+  document.querySelector(".verde").classList.remove("desligado");
 }
 
+// Ativando o Modo Automático
+function auto() {
+  document.querySelector(".vermelho").classList.remove("desligado");
+  document.querySelector(".amarelo").classList.add("desligado");
+  document.querySelector(".verde").classList.add("desligado");
 
-
-// Ativando seu Modo Automático
-let modoAutomatico = false;
-
-if (!modoAutomatico) {
-    function auto() {
-
-        document.querySelector('.vermelho').classList.remove('opacidade');
-        document.querySelector('.amarelo').classList.add('opacidade');
-        document.querySelector('.verde').classList.add('opacidade');
-    
-        // Gera um Loop
-        autoModeInterval = setInterval(function () {
-    
-            // Espaço de Tempo para o ligamento das cores
-            siga();
-            setTimeout(function () {
-                atencao();
-            }, 5000);
-            setTimeout(function () {
-                pare();
-            }, 7000);
-        }, 13500);
-    }
+  // Gera um Loop
+  autoModeInterval = setInterval(function () {
+    // Espaço de Tempo para o ligamento das cores
+    siga();
+    setTimeout(function () {
+      atencao();
+    }, 5000);
+    setTimeout(function () {
+      pare();
+    }, 7500);
+  }, 13500);
 }
-
 
 // Apagando todas as Luzes
 function reset() {
-    modoAutomatico = false
-    document.querySelector('.vermelho').classList.add('opacidade');
-    document.querySelector('.amarelo').classList.add('opacidade');
-    document.querySelector('.verde').classList.add('opacidade');
+  clearInterval(autoModeInterval);
+  document.querySelector(".vermelho").classList.add("desligado");
+  document.querySelector(".amarelo").classList.add("desligado");
+  document.querySelector(".verde").classList.add("desligado");
 }
