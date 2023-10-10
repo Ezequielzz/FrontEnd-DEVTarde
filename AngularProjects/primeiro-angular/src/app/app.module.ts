dc0139c3fe7a0034ce3e1e8472b7836227ff144b
Ezequielzz <Ezequielzz>
2023-10-10 16:56:01 -0300

    suba

--- AngularProjects/primeiro-angular/src/app/app.module.ts
+++ AngularProjects/primeiro-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { ContatoComponent } from './contato/contato.component';
+import { NgbModule } from  '@ng-bootstrap/ng-bootstrap';
 import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
