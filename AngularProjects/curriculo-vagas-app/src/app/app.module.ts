8b1e919903d718dfb9d25601f698877919ed98a3
Ezequielzz <Ezequielzz>
2023-11-01 13:44:15 -0300

    Suba

---
+++ AngularProjects/curriculo-vagas-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './view/inicio/inicio.component';
+import { CurriculosComponent } from './view/curriculos/curriculos.component';
+import { VagasComponent } from './view/vagas/vagas.component';
+import { PainelVagasComponent } from './view/painel-vagas/painel-vagas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    CurriculosComponent,
+    VagasComponent,
+    PainelVagasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
